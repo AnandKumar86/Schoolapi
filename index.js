cf555be3b31978e73236e3710b4810a5ebaabfcb
AnandKumar86 <jaiswaranandkumar437@gmail.com>
2024-12-06 22:48:50 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const db = require('./config/dbConnection');
-const addSchoolRoutes = require('./routes/addSchool');
-const listSchoolsRoutes = require('./routes/listSchool');
+const cors = require('cors');
 
 const app = express();
+const port = 3000;
+
+app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use('/addSchool', addSchoolRoutes);
-app.use('/listSchools', listSchoolsRoutes);
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'school_management'
+});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL connected...');
 });
 
 
+app.post('/addSchool', (req, res) => {
+    const { name, address, latitude, longitude } = req.body;
+
+    if (!name || !address || !latitude || !longitude) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const sql = 'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)';
+    db.query(sql, [name, address, latitude, longitude], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error' });
+        }
+        res.status(201).json({ id: result.insertId, name, address, latitude, longitude });
+    });
+});
+
+
+app.get('/listSchools', (req, res) => {
+    const { latitude, longitude } = req.query;
+
+
+    if (!latitude || !longitude) {
+        return res.status(400).json({ error: 'Latitude and longitude are required' });
+    }
+
+    const sql = 'SELECT *, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance FROM schools HAVING distance < 50 ORDER BY distance';
+    db.query(sql, [latitude, longitude, latitude], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error' });
+        }
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
