52d36f0d5f5c25ff7016eba8e4745b8dbe7978cc
AnandKumar <132542154+AnandKumar86@users.noreply.github.com>
2024-12-07 11:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ app.use(bodyParser.json());
 
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'school_management'
+   host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'school_management',
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+   
 });
 
 db.connect(err => {
